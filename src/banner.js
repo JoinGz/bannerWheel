1a41f27551695def614f3cdf312da6ab4165ddc4
Gz <354300537@qq.com>
2018-08-29 17:38:44 +0800

    begin

--- src/banner.js
+++ src/banner.js
@@ -4,18 +4,21 @@ class Banner {
       loop: true,
       LR: true,
       control: true,
-      model:'pc'
+      model: 'pc',
+      interval: 3000
     }
     this.options = Object.assign(this.default, options)
-    this.init()
     this.begin = 0
+    this.dot = 0
+    this.mytime = null
+    this.init()
   }
   init() {
-    this.$('#gzBanner .gzBox').innerHTML = this.cloneLi().innerHTML
+    this.cloneLi()
     // 设置gzBox的宽度
-    this.$('#gzBanner .gzBox').style.width = 100 * (this.num()+2) + '%'
+    this.$('#gzBanner .gzBox').style.width = 100 * this.num() + '%'
     document.querySelectorAll('#gzBanner .gzBox li').forEach(v => {
-      v.style.width = 100 / (this.num()+2) + '%'
+      v.style.width = 100 / this.num() + '%'
     })
     // 显示左右切换
     if (this.options.LR) {
@@ -35,61 +38,141 @@ class Banner {
       let ul = document.createElement('ul')
       ul.setAttribute('class', 'point')
       let li = document.createElement('li')
-      let num = this.num()
+      let num = this.num() - 1
       for (let i = 0, l = num; i < l; i++) {
         let myli = li.cloneNode()
         ul.appendChild(myli)
       }
       doc.appendChild(ul)
       document.querySelector('#gzBanner').appendChild(doc)
+      this.dotShow(0)
     }
-    if(this.options.model === 'pc'){
-      this.eventListen(this.$('#gzBanner .gzButl'),'click',()=>{
-        this.begin--
-        if (this.begin<0) {
-          this.begin = this.num()
-        }
-        this.$('#gzBanner .gzBox').style.transition = 'all 0.5s'
-        // this.$('#gzBanner .gzBox').style.transform = 'translateX('+this.begin * -this.liWidth() + 'px)'
-        this.$('#gzBanner .gzBox').style.transform = `translateX(${this.begin * -this.liWidth()}px)`
-
-      })
+    if (this.options.model === 'pc') {
+      this.eventListen(this.$('#gzBanner .gzButl'), 'click', this.gzButl())
+      this.eventListen(this.$('#gzBanner .gzButr'), 'click', this.gzButr())
     }
+    this.istime()
+    this.eventListen(this.$('#gzBanner'), 'mouseenter', () => {
+      clearInterval(this.mytime)
+    })
+    this.eventListen(this.$('#gzBanner'), 'mouseleave', () => {
+      clearInterval(this.mytime)
+      this.mytime = setInterval(() => {
+        this.gzButr()()
+      }, this.options.interval)
+    })
   }
-  liWidth(){
+  liWidth() {
     return parseInt(this.getStyle(this.$('#gzBanner .gzBox li'), 'width'))
   }
   num() {
-    return this.$('#gzBanner .gzBox').children.length - 2
+    return this.$('#gzBanner .gzBox').children.length
   }
   $(ele) {
     return document.querySelector(ele)
   }
   getStyle(obj, name) {
-    if (obj.currentStyle) { 
-      return obj.currentStyle[name];
+    if (obj.currentStyle) {
+      return obj.currentStyle[name]
     } else {
-      return getComputedStyle(obj, false)[name]; 
+      return getComputedStyle(obj, false)[name]
     }
   }
-  eventListen(obj,type,fn){
-    if(obj.addEventListener){
-      obj.addEventListener(type,fn)
-    }else if(obj.attachEvent){
+  eventListen(obj, type, fn) {
+    if (obj.addEventListener) {
+      obj.addEventListener(type, fn)
+    } else if (obj.attachEvent) {
       obj.attachEvent(`on${type}`, fn)
-    }else{
+    } else {
       obj[`on${type}`] = fn
     }
   }
-  cloneLi(){
-    let doc = document.createElement('ul')
-    let liFirst = (this.$('#gzBanner .gzBox').children[0]).cloneNode(true)
-    let last = (this.$('#gzBanner .gzBox').children[this.$('#gzBanner .gzBox').children.length-1]).cloneNode(true)
-    doc.innerHTML = this.$('#gzBanner .gzBox').innerHTML
-    doc.insertBefore(last, doc.firstChild);
-    doc.appendChild(liFirst)
-    this.$('#gzBanner .gzBox').innerHTML = ''
-    return doc
+  cloneLi() {
+    let liFirst = this.$('#gzBanner .gzBox').children[0].cloneNode(true)
+    this.$('#gzBanner .gzBox').appendChild(liFirst)
+  }
+  animation(ele, m, bool) {
+    if (bool === true) {
+      ele.style.transition = ''
+      ele.style.transform = `translateX(${m})`
+    } else if (bool === false) {
+      ele.style.transform = `translateX(${m})`
+      ele.style.transition = 'all 0.5s'
+    }
+  }
+  dotShow(num) {
+    document.querySelectorAll('#gzBanner .point li').forEach((v, i) => {
+      v.className = ''
+      if (i == num) {
+        v.className = 'active'
+      }
+    })
+  }
+  istime() {
+    // console.log(this.mytime)
+    clearInterval(this.mytime)
+    this.mytime = setInterval(() => {
+      this.gzButr()()
+    }, this.options.interval)
+    window.onfocus = ()=> {
+      // console.log('onfocue')
+      // console.log(this.mytime);
+      clearInterval(this.mytime)
+      this.mytime = setInterval(() => {
+        this.gzButr()()
+      }, this.options.interval)
+    }
+    window.onblur = ()=> {
+      // console.log('onblur');
+      // console.log(this.mytime);
+      clearInterval(this.mytime)
+    }
+  }
+  gzButl() {
+    return () => {
+      let ele = this.$('#gzBanner .gzBox')
+      this.begin--
+      this.dot--
+      if (this.begin < 0) {
+        // this.$('#gzBanner .gzBox').style.transition = ''
+        // this.$('#gzBanner .gzBox').style.transform = `translateX(${(this.num() -1) * this.liWidth() *-1}px)`
+        // this.$('#gzBanner .gzBox').style.left = `${(this.num() -1) * this.liWidth() *-1}px`
+        this.begin = this.num() - 2
+        this.animation(ele, `${(this.num() - 1) * this.liWidth() * -1}px`, true)
+      }
+      if (this.dot < 0) {
+        this.dot = this.num() - 2
+      }
+      this.animation(ele, `${this.begin * -this.liWidth()}px`, false)
+      this.dotShow(this.dot)
+      // this.$('#gzBanner .gzBox').style.transform = 'translateX('+this.begin * -this.liWidth() + 'px)'
+      // this.$('#gzBanner .gzBox').style.transition = 'all 0.5s'
+      // this.$('#gzBanner .gzBox').style.transform = `translateX(${this.begin * -this.liWidth()*-1}px)`
+    }
+  }
+  gzButr() {
+    return () => {
+      let ele = this.$('#gzBanner .gzBox')
+      this.begin++
+      this.dot++
+      if (this.begin > this.num() - 1) {
+        // let fn = () => {
+        //   this.animation(ele, `0px`, true)
+        // }
+        // if (this.begin > this.num() - 2) {
+        //   ele.addEventListener('transitionend', fn)
+        // }
+        this.animation(ele, `0px`, true)
+        this.begin = 1
+
+        // ele.removeEventListener('transitionend', fn)
+      }
+      if (this.dot > this.num() - 2) {
+        this.dot = 0
+      }
+      this.animation(ele, `${this.begin * -this.liWidth()}px`, false)
+      this.dotShow(this.dot)
+    }
   }
 }
 export default Banner
